8c4dc3cf0447851591dd2dd55f7ec3215d05e861
Artur Ostrega <skoofoo@gmail.com>
2014-03-02 15:14:02 -0600

    User-post relationship

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,5 +4,6 @@ describe Post do
   fields = %i[content topic_id]
   include_examples "has fields", fields
 
+  it { should belong_to(:user) }
   it { should belong_to(:topic) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe User do
   fields = %i[username email crypted_password salt]
   include_examples "has fields", fields
+
+  it { should have_many(:posts) }
 end
