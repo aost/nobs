6f158444279124edd1c80bc98d40f3f92b0f83aa
Artur Ostrega <skoofoo@gmail.com>
2014-03-02 22:53:01 -0600

    topic#show view style

--- spec/features/visit_topic_spec.rb
+++ spec/features/visit_topic_spec.rb
@@ -5,11 +5,11 @@ describe "visit topic" do
   include_examples "visit page"
 
   it "shows topic name" do
-    expect(page).to have_css("#topic .name", text: topic.name)
+    expect(page).to have_css("#topic-name", text: topic.name)
   end
 
   it "shows posts" do
-    post_sel = "#topic .post"
+    post_sel = "#posts .post"
 
     expect(topic.posts.count).to be > 0
     expect(page).to have_css(post_sel, count: topic.posts.count)
